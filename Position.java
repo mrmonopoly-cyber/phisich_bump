5eb93e09a30c1eae4fc918c4a9d8494713b454b5
mrmonopoly-cyber <61541968+mrmonopoly-cyber@users.noreply.github.com>
2020-10-18 22:16:16 +0200

    Update Position.java

--- Position.java
+++ Position.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.phisics_simulation;
 
 import java.util.function.BiPredicate;
